11ab2001de4e828261ea0537c7bc732ed4480e7b
Wootermelon <thibautf69220@gmail.com>
2020-12-13 19:43:16 +0100

    FIrst commit

---
+++ src/chaisse.cpp
@@ -0,0 +1,12 @@
+﻿// chaisse.cpp : définit le point d'entrée de l'application.
+//
+
+#include <iostream>
+
+// Utiliser algorithm, des lock_guard, std::array, des interfaces (PAS DE CLASSE PIECE)
+// Classes abstraites!
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
