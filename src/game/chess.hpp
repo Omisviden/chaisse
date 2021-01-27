046507a3f77a93056e7ae2640ced6e3209ab87ed
Omisviden <thibautf69220@gmail.com>
2021-01-27 12:38:29 +0100

    Forgot to include <memory> in chess.hpp

--- src/game/chess.hpp
+++ src/game/chess.hpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <iostream>
+#include <memory>
 
 #include "Piece.hpp"
 
@@ -8,8 +9,8 @@ namespace chess
 	enum class Color : bool; // Black = 0, White = 1.
 
 	void init_board(); //
-	
 	void print_board(); // Print the board state to the console.
+	
 	void move_piece(std::unique_ptr<Piece>& piece, int col, int row); // Move a piece to another square.
 
 	void play_turn(); // Play mockup turn to test move_piece();
